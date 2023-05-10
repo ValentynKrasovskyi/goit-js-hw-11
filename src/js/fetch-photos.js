3e5f8db83a6b1f51271f2e44875d357b2b0afc81
ValentynKrasovskyi <krasovskyi00@gmail.com>
2023-05-10 12:08:52 +0200

    final work

---
+++ src/js/fetch-photos.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+export async function fetchImages(searchQuery, pages = 1) {
+  const API_KEY = '35977247-3ab541937acfc9fc80c756318';
+  const BASE_URL = 'https://pixabay.com/api/';
+  const params = {
+    key: API_KEY,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+    q: searchQuery,
+    per_page: 40,
+    page: pages,
+  };
+
+  try {
+    const { data } = await axios.get(BASE_URL, { params });
+    return data;
+  } catch (error) {
+    throw error;
+  }
+}
